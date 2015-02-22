e2c27f8360374d9293e6716d7522ca94a777e3e2
Dmitry Kharitonov <geakstr@me.com>
2015-02-22 18:53:58 +0300

    Extended AABB functionality

--- src/me/geakstr/voxel/model/AABB.java
+++ src/me/geakstr/voxel/model/AABB.java
@@ -5,36 +5,26 @@ import me.geakstr.voxel.math.Vector3f;
 import java.util.Arrays;
 
 /*
-Box represents as vertices with {x,y,z} coords:
+Box represents as vertices with {x,y,z} coords in OpenGL coordinate system:
 
+              ↑ Y
+              |
+              |
+              |
+              |
      5 {0,1,0}+-------+6 {1,1,0}
            .' |     .'|
  1 {0,1,1}+-------+'4 {1,1,1}
           |   |   |   |
-     8 {0,0,0}+---|---+7 {1,0,0}
+     8 {0,0,0}+---|---+7 {1,0,0}-------------→ X
           | .'    | .'
  2 {0,0,1}+-------+'3 {1,0,1}
- 
- 
+       .'
+     .'
+   .'
+ ↙ Z
+
 All works in CCW order
-
-
-This is OpenGL coordinate system. All coordinates applied this
-                   ↑ Y
-                   |          .
-                   |        .
-                   |      .
-                   |    .
-                   |  .
-                   |.
--------------------•-------------------→ X
-                .  |
-              .    |
-            .      |
-          .        |
-        .          |
-      .            |
-    ↙  Z          |
 */
 
 public class AABB {
@@ -43,7 +33,6 @@ public class AABB {
     public Vector3f[] corners;
 
     public AABB(float x, float y, float z, float width, float height, float length) {
-
         this.corners = new Vector3f[]{
                 new Vector3f(x, y, z),
                 new Vector3f(x + width, y + height, z + length)
@@ -244,7 +233,20 @@ public class AABB {
             }
 
             public float[] tex_coords(int u, int v) {
-                return null;
+                return new float[]{
+                        // front
+                        0, 1, u, 1, 0, 0, u, 0,
+                        // back
+                        0, 1, u, 1, 0, 0, u, 0,
+                        // left
+                        v, 1, 0, 1, v, 0, 0, 0,
+                        // right
+                        v, 1, 0, 1, v, 0, 0, 0,
+                        // top
+                        0, v, u, v, 0, 0, u, 0,
+                        // bottom
+                        0, v, u, v, 0, 0, u, 0
+                };
             }
 
             public float[] translate(int x_pos, int y_pos, int z_pos) {
@@ -252,7 +254,69 @@ public class AABB {
             }
 
             public float[] translate_and_expand(int x_pos, int y_pos, int z_pos, int x_expand, int y_expand, int z_expand) {
-                return null;
+                x_expand--;
+                y_expand--;
+                z_expand--;
+                float[] side = translate(x_pos, y_pos, z_pos);
+
+                // front
+                side[3] += x_expand;
+                side[9] += x_expand;
+
+                side[7] += y_expand;
+                side[10] += y_expand;
+
+                side[2] += z_expand;
+                side[5] += z_expand;
+                side[8] += z_expand;
+                side[11] += z_expand;
+
+                // back
+                side[0 + 12] += x_expand;
+                side[6 + 12] += x_expand;
+
+                side[7 + 12] += y_expand;
+                side[10 + 12] += y_expand;
+
+                // left
+                side[7 + 24] += y_expand;
+                side[10 + 24] += y_expand;
+
+                side[5 + 24] += z_expand;
+                side[11 + 24] += z_expand;
+
+                // right
+                side[0 + 36] += x_expand;
+                side[3 + 36] += x_expand;
+                side[6 + 36] += x_expand;
+                side[9 + 36] += x_expand;
+
+                side[7 + 36] += y_expand;
+                side[10 + 36] += y_expand;
+
+                side[2 + 36] += z_expand;
+                side[8 + 36] += z_expand;
+
+                // top
+                side[3 + 48] += x_expand;
+                side[9 + 48] += x_expand;
+
+                side[1 + 48] += y_expand;
+                side[4 + 48] += y_expand;
+                side[7 + 48] += y_expand;
+                side[10 + 48] += y_expand;
+
+                side[2 + 48] += z_expand;
+                side[5 + 48] += z_expand;
+
+                // bottom
+                side[3 + 60] += x_expand;
+                side[9 + 60] += x_expand;
+
+                side[8 + 60] += z_expand;
+                side[11 + 60] += z_expand;
+
+                return side;
             }
         };
 
@@ -357,5 +421,37 @@ public class AABB {
             1, 0, 0, // v7
             0, 0, 1, // v2
             1, 0, 1, // v3
+    };
+
+    private static final float[] v1_vertex = new float[]{
+            0, 1, 1, // v1
+    };
+
+    private static final float[] v2_vertex = new float[]{
+            0, 0, 1, // v2
+    };
+
+    private static final float[] v3_vertex = new float[]{
+            1, 0, 1, // v3
+    };
+
+    private static final float[] v4_vertex = new float[]{
+            1, 1, 1, // v4
+    };
+
+    private static final float[] v5_vertex = new float[]{
+            0, 1, 0, // v5
+    };
+
+    private static final float[] v6_vertex = new float[]{
+            1, 1, 0, // v6
+    };
+
+    private static final float[] v7_vertex = new float[]{
+            1, 0, 0, // v7
+    };
+
+    private static final float[] v8_vertex = new float[]{
+            0, 0, 0, // v8
     };
 }
